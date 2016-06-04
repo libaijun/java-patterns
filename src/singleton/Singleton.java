3b96eff6f1968366720023fcd0ee2811db161ec8
libaijun <baijun1027@sina.com>
2016-06-04 15:36:17 +0800

    commit singleton

---
+++ src/singleton/Singleton.java
@@ -0,0 +1,19 @@
+package singleton;
+
+/**
+ * 单例模式
+ * 
+ * @author libojun
+ *
+ * 2016-6-4 下午3:35:37
+ */
+public class Singleton {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// test git 
+	}
+
+}
